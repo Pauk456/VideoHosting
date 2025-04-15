56f63f2ed81cf9c9ba20a472ba1d0186f6be5ac3
StubarevDmitry <146922470+StubarevDmitry@users.noreply.github.com>
2025-04-15 10:06:50 +0700

    title service initial (#2)

--- TitleService/Controller/TitleController.cs
+++ TitleService/Controller/TitleController.cs
@@ -1,14 +1,62 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using TitleService.Models;
+using TitleService.DbModels;
 
 [ApiController]
 [Route("api/title")]
 public class TitleController : Controller
 {
-    [HttpGet("specification")]
-    public async Task<ActionResult<Specification>> GetSpecification(/* Body */)
+    private readonly ApplicationDbContext _context;
+
+
+    public TitleController(ApplicationDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+
+    // получает всевозможные аниме
+    [HttpGet("all")]
+    public async Task<List<AnimeSeriesData>> GetAllAnimeSeries()
+    {
+        var response = new List<AnimeSeriesData>();
+        foreach (var item in _context.AnimeSeries)
+        {
+            response.Add(new AnimeSeriesData(item.Id, item.Title));
+        }
+        return response;
+    }
+
+    //по id сериала получает инфу о сериях и Сезонах
+    [HttpGet("getSeasonsAndEpisodes/{id}")]
+    public async Task<List<SeasonsData>> GetSeasons(int id)
+    {
+        var response = new List<SeasonsData>();
+
+        var seasons = new List<Season>();
+
+        foreach (var item in _context.Seasons)
+        {
+            if (item.SeriesId == id)
+            {
+                seasons.Add(item);
+            }
+        }
+        foreach (var season in seasons)
+        {
+            //response.Add(new AnimeSeriesData(item.Id, item.Title));
+            var episodes = new List<EpisodeData>();
+
+            foreach (var episode in _context.Episodes)
+            {
+                if (episode.SeasonId == season.Id)
+                {
+                    episodes.Add(new EpisodeData(episode.Id, episode.EpisodeNumber));
+                }
+            }
+            response.Add(new SeasonsData(season.SeasonNumber, episodes));
+        }
+        return response;
     }
 
     //тут мб лучше titleid и seasonsid и seriesid передавать через FormBody
---
+++ VideoService/Controllers/ImgController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VideoService.Controllers;
+
+using FluentFTP;
+using FluentFTP.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Renci.SshNet;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using VideoService.DbModels;
+
+using VideoService.Models;
+
+[ApiController]
+[Route("api/img")]
+public class ImgController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    private readonly HttpClient _httpClient;
+
+    public ImgController(ApplicationDbContext context, IHttpClientFactory httpClientFactory)
+    {
+        _context = context;
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    //посылается id аниме сериала чтобы получить картинку
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetImg(int id)
+    {
+        var series = await _context.AnimeSeries.FindAsync(id);
+
+        var absoluteUri = new Uri(new Uri("http://localhost:4999"), $"get-img?filePath={series.PreviewPath}");
+
+        // Используем относительный путь!
+        var response = await _httpClient.GetAsync(
+            absoluteUri,
+            HttpCompletionOption.ResponseHeadersRead
+        );
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return StatusCode((int)response.StatusCode);
+        }
+
+        return File(await response.Content.ReadAsStreamAsync(), "image/jpeg");
+    }
+}
