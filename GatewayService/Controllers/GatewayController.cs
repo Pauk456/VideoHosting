d2c47c92178c1b4da684264950017c85ee58aaeb
Vova <456pauk@mail.ru>
2025-05-24 20:42:23 +0700

    Добавл getManttitles

--- GatewayService/Controllers/GatewayController.cs
+++ GatewayService/Controllers/GatewayController.cs
@@ -7,6 +7,7 @@ using System.Text.Json;
 using System.Text;
 using Microsoft.AspNetCore.Cors;
 using static System.Net.WebRequestMethods;
+using System.IO;
 
 [EnableCors("AllowAll")]
 [ApiController]
@@ -99,8 +100,37 @@ public class GatewayController : ControllerBase
         }
     }
 
-    [HttpGet("files/{id}")]
-    
+    [HttpGet("getManyTitle/filter={filter}")]
+    public async Task<ActionResult<ManyTitleDTO>> GetManyTitle(string filter)
+    {
+        var requestedFields = filter?.Split(',')
+                            .Select(f => f.Trim())
+                            .ToHashSet(StringComparer.OrdinalIgnoreCase)
+                            ?? new HashSet<string>();
+
+        var result = new ManyTitleDTO();
+
+        try
+        {
+            if (requestedFields.Contains("reccomends"))
+                result.reccomends = await GetReccomends();
+
+            if (requestedFields.Contains("all"))
+                result.all = await GetAll();
+
+            if (requestedFields.Contains("recent"))
+                result.recent = await GetRecent();
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка про обработке запроса: getManyTitle/filter={filter}");
+            return StatusCode(500, "Internal server error");
+        }
+    }
+
+    [HttpGet("img/{id}")]
     public async Task<IActionResult> GetImgById(int id)
     {
         try
@@ -120,7 +150,7 @@ public class GatewayController : ControllerBase
         }
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("video/{id}")]
     public async Task<IActionResult> GetVideo(int id)
     {
         try
@@ -247,6 +277,68 @@ public class GatewayController : ControllerBase
             return null;
         }
     }
+
+    private async Task<JsonElement?> GetAll()
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"{ServicesAddresses.uriTitleService}/all");
+
+            response.EnsureSuccessStatusCode();
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+            using var doc = await JsonDocument.ParseAsync(stream);
+
+            return doc.RootElement.Clone();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при получении всех аниме: {ex.Message}");
+            return null;
+        }
+    }
+
+    private async Task<JsonElement?> GetReccomends()
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"{ServicesAddresses.uriRecommendationService}/top");
+
+            response.EnsureSuccessStatusCode();
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+            using var doc = await JsonDocument.ParseAsync(stream);
+
+            return doc.RootElement.Clone();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при получении рекомендованных аниме: {ex.Message}");
+            return null;
+        }
+    }
+
+    private async Task<JsonElement?> GetRecent()
+    {
+        // не реализовано то же самое что и GetReccomends
+        try
+        {
+            var response = await _httpClient.GetAsync($"{ServicesAddresses.uriRecommendationService}/top");
+
+            response.EnsureSuccessStatusCode();
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+            using var doc = await JsonDocument.ParseAsync(stream);
+
+            return doc.RootElement.Clone();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при получении рекомендованных аниме: {ex.Message}");
+            return null;
+        }
+    }
+
     private class AnimeNameResponse
     {
         public string name { get; set; }
